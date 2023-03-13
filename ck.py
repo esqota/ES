dcfb6b1d08336e325a6a4e2f73c25e30299c0376
esqota <123837501+esqota@users.noreply.github.com>
2023-03-13 18:11:59 +0200

    Add files via upload

---
+++ ck.py
@@ -0,0 +1,10 @@
+import os
+
+for filename in os.listdir():
+    if filename.endswith('.ass'):
+        ass_file = filename
+        ts_file = os.path.splitext(ass_file)[0] + '.ts'
+        if not os.path.isfile(ts_file):
+            print(f"Warning: {ts_file} does not exist for {ass_file}")
+
+print("finished")
